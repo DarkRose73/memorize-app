571d35f1b92ef13271729d899cfc763c734ead81
Joan Salas <joan.salas.silva@gmail.com>
2022-06-01 17:01:15 -0400

    commit final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useMemo, useState } from "react";
+import List from "./components/List";
+
+const initialUsers = [
+  {
+    id: 1,
+    name: "Luis",
+  },
+  {
+    id: 2,
+    name: "Maria",
+  },
+  {
+    id: 3,
+    name: "Marcos",
+  },
+];
 
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [text, setText] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleAdd = () => {
+    const newUser = { id: Date.now(), name: text };
+    const changedUsers = [...users, newUser];
+    setUsers(changedUsers);
+  };
+
+  const handleSearch = () => {
+    setSearch(text);
+  };
+
+  const handleDelete = useCallback(
+    (userId) => {
+      const changedUsers = users.filter((user) => user.id !== userId);
+      setUsers(changedUsers);
+    },
+    [users]
+  );
+
+  const filteredUsers = useMemo(
+    () =>
+      users.filter((user) => {
+        return user.name.toLowerCase().includes(search.toLowerCase());
+      }),
+    [search, users]
+  );
+
+  const printUsers = useCallback(() => {
+    console.log("changed users", users);
+  }, [users]);
+
+  useEffect(() => {
+    printUsers();
+  }, [users, printUsers]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      ></input>
+      <button onClick={handleSearch}>Search</button>
+      <button onClick={handleAdd}>Add</button>
+      <List users={filteredUsers} handleDelete={handleDelete} />
     </div>
   );
 }
